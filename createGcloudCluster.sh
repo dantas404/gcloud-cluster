c4d98116100d9e679088f3db5b2ad93560160a4a
dantas404 <dantas.nascimento.701@ufrn.edu.br>
2023-05-01 01:40:23 -0300

    Adds resource name 	modified:   createGcloudCluster.sh

--- createGcloudCluster.sh
+++ createGcloudCluster.sh
@@ -7,17 +7,19 @@ read -p "Enter the project ID: " project_id
 read -p "Enter the region: " region
 read -p "Enter the zone: " zone
 read -p "Enter the cluster name: " cluster_name
+read -p "Name the resource: " resource_name
 
 export TF_VAR_project_id=$project_id
 export TF_VAR_region=$region
 export TF_VAR_zone=$zone
 export TF_VAR_cluster_name=$cluster_name
+export TF_VAR_resource_name=$resource_name
 
 terraform init
 
 terraform plan
 
-terraform apply -var="project_id=${TF_VAR_project_id}" -var="region=${TF_VAR_region}" -var="zone=${TF_VAR_zone}" -var="cluster_name=${TF_VAR_cluster_name}" -auto-approve
+terraform apply -var="project_id=${TF_VAR_project_id}" -var="region=${TF_VAR_region}" -var="zone=${TF_VAR_zone}" -var="cluster_name=${TF_VAR_cluster_name}" -var="resource_name=${TF_VAR_resource_name}" -auto-approve
 
 gcloud container clusters get-credentials $cluster_name --zone $zone --project $project_id
 
